f9182db6bdcd51cff772a682f1fdaae1c170d963
thedeshpande <67866250+kaiwalya-ops@users.noreply.github.com>
2021-07-24 18:48:07 +0000

    added /remind function

--- main.py
+++ main.py
@@ -14,30 +14,36 @@ def greet(message):
 
 
 def remember(message):
-    request = message.text.split()
-    if len(request) == 3 and request[0].lower() == 'remember':
-        return (True)
-    return (False)  #checks if the syntax for remebering a message is correct
+	request = message.text.split()
+	if len(request) == 3 and request[0].lower() == 'remember':
+			return (True)
+	return (False)  #checks if the syntax for remebering a message is correct
 
 
 @bot.message_handler(func=remember)
 def remeb(message):
-    lst = message.text.split()
-    db[lst[1]] = lst[2]
-    bot.send_message(message.chat.id, "Gotcha!")
-    print(db[lst[1]])  #saves the data in database and replies Gotcha! to the user
+	lst = message.text.split()
+	db[lst[1]] = lst[2]
+	bot.send_message(message.chat.id, "Gotcha!")
+	print(db[lst[1]])  #saves the data in database and replies Gotcha! to the user
 
 def remind(message) :
   request = message.text.split()
   if len(request)== 2 and request[0].lower() == "remind": 
     return(True)
-  return(False)
+  return(False)  #checks if the syntax for reminding a message is correct
 
 @bot.message_handler(func= remind)
 def remi(message):
   lst = message.text.split()
   val = db[lst[1]]
   bot.send_message(message.chat.id,val)
-  print(val)
+  print(val)   #extracts the data from the database and replies it to the user
+
+@bot.message_handler(commands=['remind'])
+def remindall(message):
+	keys = db.keys()
+	for i in keys:
+		bot.send_message(message.chat.id,i)   #returns all the keys remembered by the database
 
 bot.polling()
