45bde4a6c5edf7ec618598ba77a4defdc467cf80
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2021-07-24 14:39:46 +0000

    main.py updated

--- main.py
+++ main.py
@@ -1,26 +1,43 @@
-import os #for api address
-import telebot #for the main bot
-from replit import db #for database
-key_api = os.environ['key_api'] #pulls the key from secrets
+import os  #for api address
+import telebot  #for the main bot
+from replit import db  #for database
 
-bot = telebot.TeleBot(key_api) #creats a bot linked with the api
+key_api = os.environ['key_api']  #pulls the key from secrets
 
-@bot.message_handler(commands = ["greet"]) 
+bot = telebot.TeleBot(key_api)  #creats a bot linked with the api
+
+
+@bot.message_handler(commands=["greet"])
 def greet(message):
-  bot.reply_to(message, "Hello there!") #replies Hello there! when inputted /greet
+    bot.reply_to(message,
+                 "Hello there!")  #replies Hello there! when inputted /greet
+
 
 def remember(message):
-	request = message.text.split()
-	if len(request) == 3 and request[0].lower() == 'remember':
-		return(True)
-	return(False) #checks if the syntax for remebering a message is correct
+    request = message.text.split()
+    if len(request) == 3 and request[0].lower() == 'remember':
+        return (True)
+    return (False)  #checks if the syntax for remebering a message is correct
 
-@bot.message_handler(func = remember)
+
+@bot.message_handler(func=remember)
 def remeb(message):
-  lst = message.text.split()
-  db[lst[1]] = lst[2]
-  bot.send_message(message.chat.id,"Gotcha!")
-  print(db[lst[1]]) #saves the data in database and replies Gotcha! to the user
+    lst = message.text.split()
+    db[lst[1]] = lst[2]
+    bot.send_message(message.chat.id, "Gotcha!")
+    print(db[lst[1]])  #saves the data in database and replies Gotcha! to the user
 
+def remind(message) :
+  request = message.text.split()
+  if len(request)== 2 and request[0].lower() == "remind": 
+    return(True)
+  return(False)
+
+@bot.message_handler(func= remind)
+def remi(message):
+  lst = message.text.split()
+  val = db[lst[1]]
+  bot.send_message(message.chat.id,val)
+  print(val)
 
 bot.polling()
